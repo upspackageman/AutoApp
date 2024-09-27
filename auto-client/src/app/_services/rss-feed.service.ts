b57781aa7665183c2af4578ba1eb12cc7aad9ffc
William Ward <wward246@gmail.com>
2024-09-27 13:55:29 -0700

    Remove xml2js

--- auto-client/src/app/_services/rss-feed.service.ts
+++ auto-client/src/app/_services/rss-feed.service.ts
@@ -1,30 +1,30 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import * as xml2js from 'xml2js';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { map } from 'rxjs/operators';
+// import { Observable } from 'rxjs';
+// import * as xml2js from 'xml2js';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class RssFeedService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class RssFeedService {
  
 
-  constructor(private http: HttpClient) {
+//   constructor(private http: HttpClient) {
   
-   }
-   getFeed(url: string): Observable<any> {
-    return this.http.get(url, { responseType: 'text' }).pipe(
-      map(response => {
-        let rssData: any;
-        xml2js.parseString(response, { trim: true }, (err, result) => {
+//    }
+//    getFeed(url: string): Observable<any> {
+//     return this.http.get(url, { responseType: 'text' }).pipe(
+//       map(response => {
+//         let rssData: any;
+//         xml2js.parseString(response, { trim: true }, (err, result) => {
 
-          if (!err) {
-            rssData = result;
-          }
-        });
-        return rssData;
-      })
-    );
-  }
-}
+//           if (!err) {
+//             rssData = result;
+//           }
+//         });
+//         return rssData;
+//       })
+//     );
+//   }
+// }
